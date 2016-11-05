c7e62e17ba393112ff4ed10ccda26e360aafca6e
Angus Turner <angusturner27@gmail.com>
2016-11-05 21:06:16 +1000

    added two exercises

--- staircase/main.rs
+++ staircase/main.rs
@@ -4,8 +4,19 @@ fn main() {
     // read input
     let mut line = String::new();
     io::stdin().read_line(&mut line).expect("Read error");
-    let n: i32 = line.trim().parse().expect("Parse error");
+    let n: u8 = line.trim().parse().expect("Parse error");
 
     // construct output
-    
+    let mut out = String::new();
+    for row in 0..n {
+        for _col in 0..n-row-1 {
+            out = out + " ";
+        }
+        for _col in 0..row+1 {
+            out = out + "#";
+        }
+        out = out + "\n";
+    }
+
+    println!("{}", out);
 }
