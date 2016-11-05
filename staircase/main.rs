f21dab3c2f26f5d8d367520c77e2a20e28138071
angusturner <angusturner27@gmail.com>
2016-11-05 19:15:37 +1000

    did something

---
+++ staircase/main.rs
@@ -0,0 +1,11 @@
+use std::io;
+
+fn main() {
+    // read input
+    let mut line = String::new();
+    io::stdin().read_line(&mut line).expect("Read error");
+    let n: i32 = line.trim().parse().expect("Parse error");
+
+    // construct output
+    
+}
