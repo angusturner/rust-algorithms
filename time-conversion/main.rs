8bfe9f3e16078320ef5a87da69772c5b55cf4e60
Angus Turner <angusturner27@gmail.com>
2016-11-05 21:04:05 +1000

    added time conversion exercise

---
+++ time-conversion/main.rs
@@ -0,0 +1,46 @@
+use std::io;
+
+fn main() {
+    // get input
+    let mut line = String::new();
+    io::stdin().read_line(&mut line).expect("Read error");
+
+    // extract the time portion of the input into a vector of integers vec![h:m:s]
+    let mut time: Vec<u8> = line[..8]
+        .split(":")
+        .map(str_to_u8)
+        .collect::<Vec<u8>>();
+
+    // convert 12 --> 0
+    time[0] = time[0]%12;
+
+    // if the suffix is PM, add 12 to hour integer
+    let suffix = &line[8..10];
+    match suffix {
+        "PM" => time[0] = time[0]+12,
+        _ => ()
+    };
+
+    // co-erce the time back into a string
+    let output: String = time.iter()
+        .map(u8_to_str) // convert to string and pad
+        .collect::<Vec<String>>() // collect into vector
+        .join(":"); // glue with semicolons
+
+    println!("{}", output)
+}
+
+// parse an integer to a unsigned integer
+fn str_to_u8(x: &str) -> u8 {
+    x.parse::<u8>().expect("Parse error")
+}
+
+// parse an integer to a padded string
+fn u8_to_str(x: &u8) -> String {
+    let mut out: String = x.to_string();
+    match out.len() {
+        1 => out = "0".to_string()+&out,
+        _ => ()
+    };
+    out
+}
